ddc59dc20c9b64b2d4ce06517b0609d5acdae893
0xNCORE <60969891+FullmetalNeverCore@users.noreply.github.com>
2023-05-11 03:28:48 +0300

    Add files via upload

--- space.py
+++ space.py
@@ -1,5 +1,9 @@
 # import everything from the "tri" module
 from tri import *
+from multiprocessing import Pool
+from functools import lru_cache
+from logo import *
+import os,gc
 
 # define a class called "Space"
 class Space:
@@ -16,6 +20,10 @@ class Space:
         wtl = input('Current models - cube,OBJ,few_cubes,few_objs ') # wtl - what to load
         self.load(wtl)
 
+    @lru_cache(maxsize=None)
+    def get_cube(self,x):
+       return Cube(self.app, 0, (0+x, 0-x, 0+x))
+
     # define a method to load the objects into the scene
     def load(self,wtl):
         # create 10 Cube instances, each with a unique position along the x-axis, and add them to the "obj" list
@@ -26,20 +34,22 @@ class Space:
                 self.obj.append(Twins(self.app, 2, (0, -20, -50),'default',(270,0,0)))
             case 'few_cubes':
                         print('scene might take a while to load...')
-                        self.obj = [Cube(self.app, 0, (0+x, 0-x, 0+x)) for x in range(10)]
-                        self.obj = [Cube(self.app, 0, (0-x, 0-x, 0+x)) for x in range(10)]
-            case 'few_obj':
+                        self.obj = [Cube(self.app, 0, (x, 2, y)) for x in range(-30,30,2) for y in range(-30,30,2)]
+                                  
+            case 'few_objs':
                         print('scene might take a while to load...')
-                        self.obj = [Twins(self.app, 2, (0+x, 0-x, 0+x)) for x in range(10)]
-                        self.obj = [Twins(self.app, 2, (0+x, 0-x, 0+x)) for x in range(10)] 
+                        self.obj = [Twins(self.app, 2, (0+x, 0-x, 0+x),'default',(270,0,0)) for x in range(10)]
             case _:
-                  print('the model you entered is not defined.')                
+                  print('the model you entered is not defined.')      
+        os.system('cls' if os.name=='nt' else 'clear')     
+        print(Logo.logo())     
        
 
     # define a method to remove all objects from the scene
     def destroy(self):
         # call the "destroy" method on each object in the "obj" list
         [x.destroy() for x in self.obj]
+        gc.collect()
 
     # define a method to render the objects in the scene
     def render(self):
